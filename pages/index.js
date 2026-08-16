fea96a12abc18d19471523235f0aa525a50d67d2
Manikanta <nprjwl@gmail.com>
2026-08-16 23:15:05 +0530

    changed text in home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <h1>Welcome to the Home Page and experiment successful!</h1>
       <p>This is the main landing page of the application.</p>
     </div>
   );
